c0e55534ebabda76e0ea321c2b8c1aa19a5f189a
theADAMJR <adamjf123@gmail.com>
2020-11-25 16:41:13 +0000

    first ui prototype

--- dashboard/routes/dashboard-routes.js
+++ dashboard/routes/dashboard-routes.js
@@ -13,13 +13,26 @@ router.get('/servers/:id', validateGuild,
   async (req, res) => res.render('dashboard/show', {
     savedGuild: await guilds.get(req.params.id),
     savedLog: await logs.get(req.params.id),
-    users: bot.users.cache
+    users: bot.users.cache,
+    q: [
+      {
+        title: 'Congratulations',
+        thumbnail: 'https://i1.sndcdn.com/artworks-O45qNbD6w1IH-0-t200x200.jpg',
+        author: 'Post Malone',
+        duration: '03:00'
+      },
+      {
+        title: 'Host Your Own Discord Bot Maker for FREE [24/7]',
+        thumbnail: 'https://yt3.ggpht.com/ytc/AAUvwniw3QYtqINIuqJGN8MgzpVNs6svA7aWxCfBxDfTYQ=s88-c-k-c0x00ffffff-no-rj',
+        author: 'ADAMJR',
+        duration: '05:46'
+      }
+    ]
   }));
 
 router.put('/servers/:id/:module', validateGuild, async (req, res) => {
   try {
     const { id, module } = req.params;
-
     const savedGuild = await guilds.get(id);
 
     await log.change(id, {
